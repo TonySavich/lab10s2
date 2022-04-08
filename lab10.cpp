7a80f0b5590cacd588cdeab60d127b2f0d3b3651
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-04-08 22:26:15 +0200

    Add files via upload

---
+++ lab10.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "BMP.hpp"
+
+int main()
+{
+    try
+    {
+        images::BMP test_bmp(339, 481);
+      // test_bmp.Fill({ 0,255,0 });
+
+      //  images::BMP test_bmp;
+        test_bmp.Open("int.bmp");
+        test_bmp.Rotate(acos(-1) / 4);
+        test_bmp.errordelete();
+        test_bmp.Save("out.bmp");
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
